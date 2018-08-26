4f47e1b30a3a6166ea60ae5263a2b293b50155ab
Leah Hullinger <leahhullinger@gmail.com>
2018-08-26 15:18:32 -0600

    initial commit

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr.pop();
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(family){
+  family.forEach(function(element){
+    alert(element);
+  })
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,8 +71,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
-
+function reversedLooper(letters){
+  for(let i = letters.length-1; i>=0;i--){
+    alert(letters[i])
+  }
+}
+reversedLooper(letters)
 
 ////////// PROBLEM 5 //////////
 
@@ -78,7 +90,11 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums){
+  return nums.filter(function(val){
+    return val % 2 === 0
+  })
+  }  
 
 
 
@@ -106,6 +122,20 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 */
 
 //Code Here
+function divider(numbersArray){
+  const evens = []
+  const odds = []
+
+  numbersArray.forEach(num => {
+    if(num % 2 === 0){
+      evens.push(num);
+    } 
+      else if (!num % 2 === 0){
+        odds.push(num);
+      }
+      return ([[evens],[odds]]);
+    })
+}
 
 
 
